918aa44bf81f0f844c5b9578ec23ec1b158539c0
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-08-28 14:20:59 -0300

    feat: added Profile component.

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Evelyn Schmitz</h1>
+    <h2>Desenvolvedora Fullstack</h2>
+    <p>Membro do Core Team da Empresa Taller, escrevo sobre front-end, back-end, devOps e o que vier na telha.</p>
+  </div>
+)
+
+export default Profile
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Profile from "./Profile"
 
 const Layout = ({ children }) => {
 
   return (
     <>
+      <aside>
+        <Profile />
+      </aside>
       <main>{children}</main>
     </>
   )
