23fd22affd655b10d9a66f884a89c5863130598c
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-08-28 13:27:43 -0300

    added cleanup starter stuff.

---
+++ src/components/layout.js
@@ -0,0 +1,17 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const Layout = ({ children }) => {
+
+  return (
+    <>
+      <main>{children}</main>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
