c20f2b242ffde27b93036b3e999c0c22c34d07a9
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-08-29 14:29:16 -0300

    feat: added styled-icons and SocialLinks component.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+
 import Profile from '../Profile'
+import SocialLinks from '../SocialLinks'
+
 import * as S from "./styled"
 
 const Sidebar = () => (
   <S.SidebarWrapper>
     <Profile />
+    <SocialLinks />
   </S.SidebarWrapper>
 )
 
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/'
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/'
+  },
+  {
+    label: 'Youtube',
+    url: 'https://youtube.com/'
+  },
+  {
+    label: 'Instagram',
+    url: 'https://instagram.com/'
+  },
+  {
+    label: 'Unsplash',
+    url: 'https://Unsplash.com/'
+  }
+]
+
+export default links
