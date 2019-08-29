87dedfb147e89a4bdecabda92fa0bb78938d467c
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-08-28 23:47:11 -0300

    feat: added Avataar component.

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+import Avatar from "../Avatar";
+
 const Profile = () => {
   const {
     site: {
@@ -24,6 +26,7 @@ const Profile = () => {
 
     return (
       <div className="Profile-wrapper">
+        <Avatar />
         <h1>{title}</h1>
         <h2>{position}</h2>
         <p>{description}</p>
