e11be89751460ba64ad1d99199781aa3754230a1
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-08-29 14:01:17 -0300

    feat: added sidebar component.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,18 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Profile from "../Profile"
+import Sidebar from "../Sidebar"
 
 import *  as S from "./styled"
-import GlobalStyles from '../../styles/global'
+import GlobalStyles from "../../styles/global"
 
 const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
       <GlobalStyles />
-      <aside>
+      <Sidebar>
         <Profile />
-      </aside>
+      </Sidebar>
       <S.LayoutMain>{children}</S.LayoutMain>
     </S.LayoutWrapper>
   )
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Profile from '../Profile'
+import * as S from "./styled"
+
+const Sidebar = () => (
+  <S.SidebarWrapper>
+    <Profile />
+  </S.SidebarWrapper>
+)
+
+export default Sidebar
