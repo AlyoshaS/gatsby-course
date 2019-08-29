0c8af084668546b3f94bad432b73883d129699ef
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-08-29 00:00:02 -0300

    feat: added styled components.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `Evelyn Schmitz`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
