861c6dafeabe7853741f95e0063660bb374acd23
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-08-29 15:26:12 -0300

    feat: added MenuLinks component.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Profile from '../Profile'
 import SocialLinks from '../SocialLinks'
+import MenuLinks from '../MenuLinks'
 
 import * as S from "./styled"
 
@@ -9,6 +10,7 @@ const Sidebar = () => (
   <S.SidebarWrapper>
     <Profile />
     <SocialLinks />
+    <MenuLinks />
   </S.SidebarWrapper>
 )
 
