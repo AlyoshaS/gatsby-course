31e0720644ffa1dfbab6a8b601f374d608cfc0c6
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-08-30 23:25:29 -0300

    feat: added gatsby-transform-remark.

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,21 @@ module.exports = {
         path: `${__dirname}/posts`,
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
