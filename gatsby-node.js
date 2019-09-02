c93e1666b46ec0c054bc723756236c513d3582b0
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-09-01 22:44:42 -0300

    feat: added postsPerPage configs to be used in blog-list.

--- gatsby-node.js
+++ gatsby-node.js
@@ -44,12 +44,32 @@ exports.createPages = ({ graphql, actions }) => {
       }
     }
   `).then(result => {
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve("./src/templates/blog-post.js"),
         context: {
           slug: node.fields.slug
+        }
+      })
+    })
+
+    const postsPerPage = 6
+    const numPages = Math.ceil(posts.length / postsPerPage)
+
+    Array.from({ length: numPages }).forEach((_, index) => {
+      createPage({
+        path: index === 0 ? `/` : `/page/${index + 1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
+        context: {
+        // Data passed to context is avaliable in page queries 
+        // as gql variables 
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          numPages,
+          currentPage: index + 1  
         }
       })
     })
