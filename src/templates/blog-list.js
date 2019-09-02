b31b11131b9632548ddc25804239736370fd0d23
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-09-01 23:40:15 -0300

    feat: added Pagination component \o/.

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -4,9 +4,15 @@ import { graphql } from 'gatsby'
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
+import Pagination from "../components/Pagination"
 
 const BlogList = props => {
   const post = props.data.allMarkdownRemark.edges
+  const { currentPage, numPages } = props.pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? `/` : `/page/${currentPage - 1}`
+  const nextPage = `/page/${currentPage + 1}`
   
   return (
     <Layout>
@@ -28,6 +34,15 @@ const BlogList = props => {
           description={description}
         />
       ))}
+
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        currentPage={currentPage}
+        numPages={numPages}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
     </Layout>
   )
 }
