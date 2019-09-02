3c3ce8ded6aae41efaee33c1387337bb1b6a5772
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-09-02 00:45:32 -0300

    feat: added Post component.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+import * as S from "../components/Post/styled"
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
   
   return (
-    <>
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{__html: post.html}}></div>
-    </>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
+        </S.PostDate>
+        <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
+        <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{__html: post.html}}></div>
+      </S.MainContent>
+    </Layout>
   )
 }
 
@@ -21,8 +35,11 @@ export const query = graphql`
     }) {
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
       html
+      timeToRead
     }
   }
 `
